9e818601f8879df016176a7d80e7f0fc846fdc59
kjirou <sorenariblog@gmail.com>
2018-03-26 19:50:30 +0900

    生webpack.config.jsをみるためのダンプ用スクリプトをメモ

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+//console.log(JSON.stringify(environment.toWebpackConfig().toObject(), null, 2));
+
 module.exports = environment.toWebpackConfig()
