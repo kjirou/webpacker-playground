1983df91e3a2408f0447312e80fe372ddd75a4e0
kjirou <sorenariblog@gmail.com>
2018-03-26 16:36:15 +0900

    現在のnpm packageツリーに含まれないパッケージをaddして呼び出してみる
    ```
    ./bin/yarn add sort-array-for-drag-and-drop
    ```
    
    呼び出せた。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,6 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
+
+const {moveElement} = require('sort-array-for-drag-and-drop');
+console.log('moveElement:', moveElement.toString());
